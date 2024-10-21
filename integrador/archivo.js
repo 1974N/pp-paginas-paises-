411240118f3d49eaf045e28b0d225a4352638899
1974N <164445662+1974N@users.noreply.github.com>
2024-10-21 16:56:20 -0300

    archivo.js

--- integrador/archivo.js
+++ integrador/archivo.js
@@ -1,7 +1,8 @@
-const paisesData = []
-document.getElementById('buscar').addEventListener('click', function() {
-    const countryName = document.getElementById('entrada').value;
+const paisesData = [];
 
+document.getElementById('buscar')
+addEventListener('click', function() {
+    const countryName = document.getElementById('entrada').value.trim(); // Elimina espacios innecesarios
 
     if (countryName) {
         fetch(`https://restcountries.com/v3.1/name/${countryName}`)
@@ -12,39 +13,35 @@ document.getElementById('buscar').addEventListener('click', function() {
                 return response.json();
             })
             .then(data => {
-                const countryInfo = data[0]; 
+                const countryInfo = data[0];
 
+                if (countryInfo) {
+                    const flag = countryInfo.flags.png; 
+                    const name = countryInfo.name.common; 
+                    const capital = countryInfo.capital ? countryInfo.capital[0] : 'No disponible'; 
+                    const region = countryInfo.region; 
+                    const subregion = countryInfo.subregion; 
+                    const population = countryInfo.population; 
+                    const area = countryInfo.area; 
+                    const languages = countryInfo.languages ? Object.values(countryInfo.languages).join(', ') : 'No disponible'; 
+                    const currencies = countryInfo.currencies ? Object.values(countryInfo.currencies).map(currency => currency.name).join(', ') : 'No disponible'; 
 
-                
-                const flag = countryInfo.flags.png; 
-                const name = countryInfo.name.common; 
-                const capital = countryInfo.capital ? countryInfo.capital[0] : 'No disponible'; 
-                const region = countryInfo.region; 
-                const subregion = countryInfo.subregion; 
-                const population = countryInfo.population; 
-                const area = countryInfo.area; 
-                const languages = Object.values(countryInfo.languages).join(', '); 
-                const currencies = Object.values(countryInfo.currencies).map(currency => currency.name).join(', '); 
-
-
-                document.getElementById('paisNombre').innerText = name;
-                document.getElementById('bandera').src = flag;
-                document.getElementById('paisCapital').innerText = capital;
-                document.getElementById('paisRegion').innerText = region;
-                document.getElementById('paisSubregion').innerText = subregion;
-                document.getElementById('paisPoblacion').innerText = population;
-                document.getElementById('paisArea').innerText = area;
-                document.getElementById('paisIdiomas').innerText = languages;
-                document.getElementById('paisMonedas').innerText = currencies;
+                    document.getElementById('paisNombre').innerText = name;
+                    document.getElementById('bandera').src = flag;
+                    document.getElementById('paisCapital').innerText = capital;
+                    document.getElementById('paisRegion').innerText = region;
+                    document.getElementById('paisSubregion').innerText = subregion;
+                    document.getElementById('paisPoblacion').innerText = population;
+                    document.getElementById('paisArea').innerText = area;
+                    document.getElementById('paisIdiomas').innerText = languages;
+                    document.getElementById('paisMonedas').innerText = currencies;
+                    document.getElementById('resultado').innerHTML = ''; // Limpiar resultados anteriores
+                }
             })
             .catch(error => {
-                
                 document.getElementById('resultado').innerHTML = `<p style="color: red;">${error.message}</p>`;
             });
     } else {
         document.getElementById('resultado').innerHTML = '<p style="color: red;">Por favor, ingresa un país.</p>';
     }
 });
-
-
-
